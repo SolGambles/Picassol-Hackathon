4c16a281f66375b148c63a122859434a4974bb4f
Sterling <162571475+Sterling42@users.noreply.github.com>
2024-03-13 00:04:32 +0100

    Update Canvas.tsx
    removed Pixel borders

--- frontend/components/Canvas.tsx
+++ frontend/components/Canvas.tsx
@@ -88,10 +88,10 @@ export default function Canvas({ program, selectedColor }: Props) {
   return (
     <div className={clsx(disabled && "opacity-25 cursor-not-allowed")}>
       <table className="border border-gray-300 table-fixed">
-        <tbody className="divide-y divide-gray-300">
+        <tbody className="">
           {[...Array(100)].map((_, y) => {
             return (
-              <tr className="divide-x divide-gray-300" key={y}>
+              <tr className="" key={y}>
                 {[...Array(100)].map((_, x) => {
                   const id = y * 100 + x;
                   const pixelData = pixelsMap[id];
