70985694b4f3f6a2d084be7279800f5b5296b669
Sterling <162571475+Sterling42@users.noreply.github.com>
2024-03-13 06:22:56 +0100

    Update Navbar.tsx
    changed spacing, added pages

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,22 +1,42 @@
-// Navbar.tsx
-
-import React from "react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React from 'react';
+import Link from 'next/link';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Picassol.art</div>
-        <ul className="flex items-center">
-          <li className="mr-4">
+        <div className="flex items-center space-x-8">
+          <div>
+            <div className="text-white font-bold text-xl">Picassol.art</div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div>
+              <Link href="/">
+                <a className="text-white hover:text-gray-300">Canvas</a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/mint">
+                <a className="text-white hover:text-gray-300">Mint</a>
+              </Link>
+            </div>
+            <div>
+              <Link href="https://picassol.gitbook.io/picassol">
+                <a className="text-white hover:text-gray-300">Docs</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <ul className="flex items-center space-x-4">
+          <li>
             <a href="https://twitter.com/PicassolCanvas" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Twitter</a>
           </li>
-          <li className="mr-4">
+          <li>
             <a href="https://discord.gg/M7XQuDB59y" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Discord</a>
           </li>
-          <li className="mr-4">
-            <a href="https://picassol.gitbook.io/picassol" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Docs</a>
+          <li>
+            <a href="https://t.me/picassol" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Telegram</a>
           </li>
           <li>
             <WalletMultiButton className="bg-black hover:bg-black text-white" />
