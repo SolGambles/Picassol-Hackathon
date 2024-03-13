ce278c244e03ac3946cce5a48842fd751ced79e3
Sterling <162571475+Sterling42@users.noreply.github.com>
2024-03-13 03:44:37 +0100

    Update Title.tsx

--- frontend/components/Title.tsx
+++ frontend/components/Title.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 export default function Title({ children }) {
   return (
-    <h1 className="text-center text-5xl font-bold font-sans text-black">
+    <h1 className="text-center text-5xl font-bold font-sans text-black p-12">
       {children}
     </h1>
   );
