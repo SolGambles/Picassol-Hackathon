618fcd9be52d24553391b8243c9d3fc06d68e874
Sterling <162571475+Sterling42@users.noreply.github.com>
2024-03-13 04:01:54 +0100

    Update index.tsx
    Added footer

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,52 +1,54 @@
-import Navbar from "../components/Navbar"; // Import the Navbar component
+import React, { useMemo, useState } from "react";
+import Navbar from "../components/Navbar";
 import ColorSelector from "../components/ColorSelector";
 import Title from "../components/Title";
 import { Color, colors } from "../lib/colors";
-import { useMemo, useState } from "react";
-import Canvas from "../components/Canvas";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { AnchorWallet, useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import { DrawWithFrens, IDL } from "../idl/draw_with_frens";
 import { PublicKey } from "@solana/web3.js";
+import Canvas from "../components/Canvas";
+import Footer from "../components/Footer";
 
 export default function Home() {
-  const [selectedColor, setSelectedColor] = useState<Color>(colors[0])
+  const [selectedColor, setSelectedColor] = useState<Color>(colors[0]);
 
   const { connection } = useConnection();
 
-  const anchorWallet: AnchorWallet | undefined = useAnchorWallet()
+  const anchorWallet: AnchorWallet | undefined = useAnchorWallet();
 
   const anchorProvider: AnchorProvider | undefined = useMemo(() => {
     if (anchorWallet) {
-      return new AnchorProvider(connection, anchorWallet, { commitment: 'confirmed' })
+      return new AnchorProvider(connection, anchorWallet, { commitment: 'confirmed' });
     } else {
-      return undefined
+      return undefined;
     }
-  }, [connection, anchorWallet])
+  }, [connection, anchorWallet]);
 
   // Make sure you use your program ID!
   const programId = new PublicKey("p1Z5CJa9U1e3XU9Uq9zhCQArD19MidRfBdMHPowtoim");
 
   const anchorProgram: Program<DrawWithFrens> | undefined = useMemo(() => {
     if (anchorProvider) {
-      return new Program(IDL, programId, anchorProvider)
+      return new Program(IDL, programId, anchorProvider);
     } else {
-      return undefined
+      return undefined;
     }
-  }, [anchorProvider])
+  }, [anchorProvider]);
 
   return (
     <div>
-      <Navbar /> {/* Add the Navbar component */}
+      <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen">
         <Title>Picassol Canvas Season 0</Title>
         <div className="mb-8">
-          <WalletMultiButton className="!bg-gray-900 hover:scale-105" />
+          <WalletMultiButton className="bg-black hover:bg-black" />
         </div>
         <ColorSelector selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
         <Canvas program={anchorProgram} selectedColor={selectedColor} />
       </div>
+      <Footer />
     </div>
   );
 }
