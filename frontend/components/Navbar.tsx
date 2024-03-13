52c4f3e96dd0ab8bbb63bd7b1d150602f671c970
Sterling <162571475+Sterling42@users.noreply.github.com>
2024-03-13 04:09:55 +0100

    Update Navbar.tsx
    added connect wallet button

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,21 +1,25 @@
 // Navbar.tsx
 
 import React from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white font-bold text-xl">Picassol.art</div>
-        <ul className="flex">
+        <ul className="flex items-center">
           <li className="mr-4">
             <a href="https://twitter.com/PicassolCanvas" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Twitter</a>
           </li>
           <li className="mr-4">
             <a href="https://discord.gg/M7XQuDB59y" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Discord</a>
           </li>
-          <li>
+          <li className="mr-4">
             <a href="https://picassol.gitbook.io/picassol" className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer">Docs</a>
+          </li>
+          <li>
+            <WalletMultiButton className="bg-black hover:bg-black text-white" />
           </li>
         </ul>
       </div>
