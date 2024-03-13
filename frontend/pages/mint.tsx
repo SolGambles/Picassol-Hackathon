faf1d3c082d9186c2b7cc80cb1097df709707966
Sterling <162571475+Sterling42@users.noreply.github.com>
2024-03-13 06:23:28 +0100

    Create mint.tsx

---
+++ frontend/pages/mint.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components//Navbar';
+import Footer from '../components/Footer';
+
+const MintPage: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Mint Page</h1>
+      {/* Add your minting functionality here */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MintPage;
