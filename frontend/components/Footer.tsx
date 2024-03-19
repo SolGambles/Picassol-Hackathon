e738c38e883cca537a9441f53b4577ccbdc09ded
Guney <devtex16@gmail.com>
2024-03-19 16:40:30 +0300

    Update social media links and copyright year in Footer component

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -6,18 +6,44 @@ function Footer() {
     <footer className="p-4 bg-black">
       <div className="container mx-auto px-4 flex flex-col justify-between items-start">
         <div className="flex items-center space-x-4 mb-4">
-          <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
-            <img src="/discord.png" alt="Discord" className="h-6 w-6" />
+          <a
+            href="https://discord.gg/M7XQuDB59y"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/discord.png"
+              alt="Discord"
+              className="h-6 w-6 hover:scale-110"
+            />
           </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src="/twitter.png" alt="Twitter" className="h-6 w-6" />
+          <a
+            href="https://twitter.com/PicassolCanvas"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/twitter.png"
+              alt="Twitter"
+              className="h-6 w-6 hover:scale-110"
+            />
           </a>
-          <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
-            <img src="/telegram.png" alt="Telegram" className="h-6 w-6" />
+          <a
+            href="https://telegram.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/telegram.png"
+              alt="Telegram"
+              className="h-6 w-6 hover:scale-110"
+            />
           </a>
         </div>
         <div className="flex justify-between items-center w-full">
-          <p className="text-white">&copy; 2024 Picassol.art, All rights reserved.</p>
+          <p className="text-white">
+            &copy; 2024 Picassol.art, All rights reserved.
+          </p>
           <div>
             <a
               href="/privacy-policy"
